a263c6a0d77cd509e626546fc6e877c96776031e
Dustin <dustinsinkey1@gmail.com>
2016-02-10 21:12:07 -0800

    Added button and classes to Flux Product component

--- js/components/FluxProduct.react.js
+++ js/components/FluxProduct.react.js
@@ -3,17 +3,14 @@ var FluxCartActions = require('../actions/FluxCartActions.js');
 
 var FluxProduct = React.createClass({
   render: function(){
-    var fromFluxProduct = this.props.product;
-    console.log("This is from FluxProductComponent:");
-    console.log(fromFluxProduct);
-
     return(
-      <div>
+      <div className="flux-product">
         <img src={'img/' + this.props.product.image}></img>
-        <div>
-          <h1>{this.props.product.name}</h1>
-          <p>{this.props.product.description}</p>
-          <p>{this.props.product.price}</p>
+        <div className="flux-product-detail">
+          <h1 className="name">{this.props.product.name}</h1>
+          <p className="description">{this.props.product.description}</p>
+          <p className="price">{this.props.product.price}</p>
+          <button type="button">Add to Cart</button>
         </div>
       </div>
     )
